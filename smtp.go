aa346f01eec374ca6a5e9b02cf5ee3da64693870
Joshua Poehls <joshua@zduck.com>
2013-08-01 21:46:51 -0500

    Tweaked comment.

--- smtp.go
+++ smtp.go
@@ -5,8 +5,7 @@ import (
 )
 
 // SendMail connects to the server at addr, switches to TLS if possible,
-// authenticates with mechanism a if possible, and then sends an email from
-// address from, to addresses to, with message msg.
+// authenticates with mechanism a if possible, and then sends the given Message.
 //
 // Based heavily on smtp.SendMail().
 func SendMail(addr string, a smtp.Auth, msg *Message) error {
